724a7607f3fd7ed43651f05df2918a5021f19afe
Ivan Benitez <ivanmatias300@gmail.com>
2025-02-10 23:31:09 -0300

    First commit

---
+++ app.js
@@ -0,0 +1,45 @@
+
+let amigos = [];
+
+function agregarAmigo() {
+    let nombreDeAmigo = document.getElementById("amigo").value;
+
+    if( nombreDeAmigo == "") {
+        alert("Por favor, inserte un nombre.")
+    } else {
+        amigos.push(nombreDeAmigo);
+        limpiarInput();
+        mapeoDeAmigos();
+    }
+
+    return;
+}
+
+function sortearAmigo() {
+    if (amigos.length > 0) {
+        let amigoElegido = Math.floor(Math.random() * amigos.length);
+        let amigoSecreto = amigos[amigoElegido];
+
+        let mostrarAmigoSecreto = document.getElementById("resultado");
+        mostrarAmigoSecreto.innerHTML = amigoSecreto;
+
+    } else {
+        alert("Agregar un amigo antes de sortear");
+    }
+}
+
+function mapeoDeAmigos() {
+    let listaAmigos = document.getElementById("listaAmigos");
+    listaAmigos.innerHTML = "";
+
+    for (let i = 0; i < amigos.length; i++) {
+        let elemento = document.createElement("li");
+        elemento.innerHTML = amigos[i];
+        listaAmigos.appendChild(elemento);
+    }
+}
+
+function limpiarInput() {
+    document.getElementById("amigo").value = "";
+    return;
+}
